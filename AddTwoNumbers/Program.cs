beb40850e8721a47919d765c6a0e639f03bb2a26
unknown <jimpidas>
2021-05-17 19:06:45 +0530

    [Jyosmita]Added: Addition of two number using MS test and .Net framework

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        public int Add(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+    }
+}
+
